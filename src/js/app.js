1767c742446332f6e4566007fea82257bd11adba
Daniel Ignaciuk <daniel.ignaciuk@gmail.com>
2022-01-13 12:58:40 +0100

    Create subpages

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,32 @@ const app = {
     const thisApp = this;
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
-    thisApp.activatePage(thisApp.pages[0].id);
+    
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+    
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', ''); 
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
     
   },
 
@@ -40,7 +65,7 @@ const app = {
     const thisApp = this;
     thisApp.data = {};
     const url = settings.db.url + '/' + settings.db.products;
-
+    
     fetch(url)
       .then(function(rawResponse){
         return rawResponse.json();
@@ -48,13 +73,15 @@ const app = {
       .then(function(parsedResponse){
         console.log('parsedResponse:', parsedResponse);
 
-        /* save parsedResponse as thisApp.data.products */
+        //save parsedResponse as thisApp.data.products 
         thisApp.data.products = parsedResponse;
 
-        /* execute initMenu method */
+        //execute initMenu method 
         thisApp.initMenu();
       });
     console.log('thisApp.data', JSON.stringify(thisApp.data));
+    
+    
   },
 
   initCart: function(){
@@ -75,7 +102,7 @@ const app = {
     //console.log('classNames:', classNames);
     //console.log('settings:', settings);
     //console.log('templates:', templates);
-    thisApp.initPages();
+    
     thisApp.initData();
     thisApp.initCart();
   },
